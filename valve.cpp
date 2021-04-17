ebe9f1532847c231fb252482a140538af01b5086
Sebastian <online.internet@mailbox.org>
2021-04-17 08:14:50 +0200

    Valve: Add enabled state

--- valve.cpp
+++ valve.cpp
@@ -12,6 +12,27 @@ Valve::Valve(std::string name, uint8_t pin, int16_t waterDuration)
     this->waterDuration = waterDuration;
 }
 
+Valve::Valve(std::string name, uint8_t pin, int16_t waterDuration, bool enabled)
+:Valve(name, pin, waterDuration)
+{
+    this->enabled = enabled;
+}
+
+void Valve::enable()
+{
+    this->enabled = true;
+}
+
+void Valve::disable()
+{
+    this->enabled = false;
+}
+
+bool Valve::isEnabled()
+{
+    return this->enabled;
+}
+
 Valve::~Valve()
 {
     
--- valve.h
+++ valve.h
@@ -8,11 +8,16 @@ class Valve : public Aktor
 {
 private:
 int16_t waterDuration;
+bool enabled = false;
 
 public:
 Valve();
 Valve(std::string name, uint8_t pin, int16_t waterDuration);
+Valve(std::string name, uint8_t pin, int16_t waterDuration, bool enabled);
 ~Valve();
+void enable();
+void disable();
+bool isEnabled();
 
 };
 
